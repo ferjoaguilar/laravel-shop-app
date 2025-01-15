a51ec5e69a4a3a8fb0f8163e85af9ef5eb136de1
Fernando Aguilar <feraguilar6985@gmail.com>
2025-01-14 18:10:49 -0600

    Add Swagger documentation and user API endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Info(
+ *    title="Shop API",
+ *    version="1.0.0",
+ *    description="API for shop"
+ * )
+ */
+
+
 class UserController extends Controller
 {
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/users",
+     *     summary="Get all users",
+     *     tags={"Users"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="get all users",
+     *     )
+     * )
+     */
     public function getAllUsers(Request $request)
     {
         return response()->json([
